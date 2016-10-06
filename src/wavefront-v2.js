8afb1b9b61b856fef03b8895e8810d9e6f145fbb
Julius <j8@protonmail.com>
2016-10-06 20:27:12 +0100

    Removed libs folder

--- src/wavefront-v2.js
+++ src/wavefront-v2.js
@@ -1,13 +1,6 @@
 import classList from './polyfills/class-list';
 import { contains, once } from './utils';
 import { render, addInterfaceRenderMethod } from './render';
-import {
-    patch,
-    elementOpen,
-    elementClose,
-    elementVoid,
-    text
-} from '../libs/incremental-dom-es6';
 // const waveFront = () => {
 //     window.addEventListener('load', function() {
 //         if (window.WAVEFRONT_ENV === 'dev') {
@@ -20,35 +13,9 @@ import {
 
 let store = {};
 
-// render.dynamic['foo'] = function(){
-//     this.method1();
-//     this.method2();
-// }
-
-
+// TESTING.
 window.render = render;
-// function MyClass(){
-//     this.water = 'refreshing';
-// }
-// MyClass.prototype = {
 
-//     method1: ()=>{
-//         console.log('method1',this.water)
-//     },
-//     method2: ()=>{
-//         console.log('method2',this.water)
-//     }
-// }
-
-
-// render.dynamic = MyClass.prototype
-// render.stateless = MyClass.prototype
-// render.static = MyClass.prototype
-
-// render.stateless['face'] = ()=>{
-//     console.log(this)
-// }
-// var render = {}
 
 export function __(interfaceNamewaveName) {
     let props = {};
@@ -98,15 +65,7 @@ export function __(interfaceNamewaveName) {
             element.addEventListener(event, handler, bubbling)
         }
         return props;
-    }
-
-
-
-
-    // return {
-
-    // } 
-    else {
+    } else {
         throw new Error('The interface name and wave Id should be separated by a colon.')
     }
 
@@ -294,25 +253,6 @@ export function renderBranches(tree, selector, interfaceName) {
     }
 }
 
-
-
-
-// function render(interfaceName, selector) {
-
-
-//     let currentVirtualTree = __._dynamicStore[interfaceName].currentVirtualTree();
-//     // if (_renderTree.prototype[interfaceName]) {
-//     //     //
-//     // } else {
-//     //     _renderTree.prototype[interfaceName] = true;
-
-
-
-//     console.log('new interface created')
-//     renderBranches(currentVirtualTree, selector, interfaceName);
-//     // }
-// }
-
 __._registerDynamicInterface = function(interFace, dynamicScope, interfaceName) {
     if (once('_registerDynamicInterface' + interfaceName)) {
         __.model[interfaceName]['name'] = interfaceName;
@@ -392,106 +332,114 @@ var assembly = (tagName) => {
 __.render = render;
 
 
-export var a = assembly('a');
-export var abbr = assembly('abbr');
-export var address = assembly('address');
-export var area = assembly('area');
-export var article = assembly('article');
-export var aside = assembly('aside');
-export var audio = assembly('audio');
-export var b = assembly('b');
-export var base = assembly('base');
-export var bdi = assembly('bdi');
-export var bdo = assembly('bdo');
-export var blockquote = assembly('blockquote');
-export var body = assembly('body');
-export var br = assembly('br');
-export var button = assembly('button');
-export var canvas = assembly('canvas');
-export var caption = assembly('caption');
-export var cite = assembly('cite');
-export var code = assembly('code');
-export var col = assembly('col');
-export var colgroup = assembly('colgroup');
-export var command = assembly('command');
-export var dd = assembly('dd');
-export var del = assembly('del');
-export var dfn = assembly('dfn');
-export var div = assembly('div');
-export var dl = assembly('dl');
-export var doctype = assembly('doctype');
-export var dt = assembly('dt');
-export var em = assembly('em');
-export var embed = assembly('embed');
-export var fieldset = assembly('fieldset');
-export var figcaption = assembly('figcaption');
-export var figure = assembly('figure');
-export var footer = assembly('footer');
-export var form = assembly('form');
-export var h1 = assembly('h1');
-export var h2 = assembly('h2');
-export var h3 = assembly('h3');
-export var h4 = assembly('h4');
-export var h5 = assembly('h5');
-export var h6 = assembly('h6');
-export var header = assembly('header');
-export var hgroup = assembly('hgroup');
-export var hr = assembly('hr');
-export var html = assembly('html');
-export var i = assembly('i');
-export var iframe = assembly('iframe');
-export var img = assembly('img');
-export var input = assembly('input');
-export var ins = assembly('ins');
-export var kbd = assembly('kbd');
-export var keygen = assembly('keygen');
-export var label = assembly('label');
-export var legend = assembly('legend');
-export var li = assembly('li');
-export var link = assembly('link');
-export var map = assembly('map');
-export var mark = assembly('mark');
-export var menu = assembly('menu');
-export var meta = assembly('meta');
-export var nav = assembly('nav');
-export var noscript = assembly('noscript');
-export var object = assembly('object');
-export var ol = assembly('ol');
-export var optgroup = assembly('optgroup');
-export var option = assembly('option');
-export var p = assembly('p');
-export var param = assembly('param');
-export var pre = assembly('pre');
-export var progress = assembly('progress');
-export var q = assembly('q');
-export var rp = assembly('rp');
-export var rt = assembly('rt');
-export var ruby = assembly('ruby');
-export var s = assembly('s');
-export var samp = assembly('samp');
-export var script = assembly('script');
-export var section = assembly('section');
-export var select = assembly('select');
-export var small = assembly('small');
-export var source = assembly('source');
-export var span = assembly('span');
-export var strong = assembly('strong');
-export var style = assembly('style');
-export var sub = assembly('sub');
-export var sup = assembly('sup');
-export var table = assembly('table');
-export var tbody = assembly('tbody');
-export var td = assembly('td');
-export var textarea = assembly('textarea');
-export var tfoot = assembly('tfoot');
-export var th = assembly('th');
-export var thead = assembly('thead');
-export var title = assembly('title');
-export var tr = assembly('tr');
-export var ul = assembly('ul');
-export var v = assembly('var');
-export var video = assembly('video');
+export const a = assembly('a');
+export const abbr = assembly('abbr');
+export const address = assembly('address');
+export const area = assembly('area');
+export const article = assembly('article');
+export const aside = assembly('aside');
+export const audio = assembly('audio');
+export const b = assembly('b');
+export const base = assembly('base');
+export const bdi = assembly('bdi');
+export const bdo = assembly('bdo');
+export const blockquote = assembly('blockquote');
+export const body = assembly('body');
+export const br = assembly('br');
+export const button = assembly('button');
+export const canvas = assembly('canvas');
+export const caption = assembly('caption');
+export const cite = assembly('cite');
+export const code = assembly('code');
+export const col = assembly('col');
+export const colgroup = assembly('colgroup');
+export const command = assembly('command');
+export const dd = assembly('dd');
+export const del = assembly('del');
+export const dfn = assembly('dfn');
+export const div = assembly('div');
+export const dl = assembly('dl');
+export const doctype = assembly('doctype');
+export const dt = assembly('dt');
+export const em = assembly('em');
+export const embed = assembly('embed');
+export const fieldset = assembly('fieldset');
+export const figcaption = assembly('figcaption');
+export const figure = assembly('figure');
+export const footer = assembly('footer');
+export const form = assembly('form');
+export const h1 = assembly('h1');
+export const h2 = assembly('h2');
+export const h3 = assembly('h3');
+export const h4 = assembly('h4');
+export const h5 = assembly('h5');
+export const h6 = assembly('h6');
+export const header = assembly('header');
+export const hgroup = assembly('hgroup');
+export const hr = assembly('hr');
+export const html = assembly('html');
+export const i = assembly('i');
+export const iframe = assembly('iframe');
+export const img = assembly('img');
+export const input = assembly('input');
+export const ins = assembly('ins');
+export const kbd = assembly('kbd');
+export const keygen = assembly('keygen');
+export const label = assembly('label');
+export const legend = assembly('legend');
+export const li = assembly('li');
+export const link = assembly('link');
+export const map = assembly('map');
+export const mark = assembly('mark');
+export const menu = assembly('menu');
+export const meta = assembly('meta');
+export const nav = assembly('nav');
+export const noscript = assembly('noscript');
+export const object = assembly('object');
+export const ol = assembly('ol');
+export const optgroup = assembly('optgroup');
+export const option = assembly('option');
+export const p = assembly('p');
+export const param = assembly('param');
+export const pre = assembly('pre');
+export const progress = assembly('progress');
+export const q = assembly('q');
+export const rp = assembly('rp');
+export const rt = assembly('rt');
+export const ruby = assembly('ruby');
+export const s = assembly('s');
+export const samp = assembly('samp');
+export const script = assembly('script');
+export const section = assembly('section');
+export const select = assembly('select');
+export const small = assembly('small');
+export const source = assembly('source');
+export const span = assembly('span');
+export const strong = assembly('strong');
+export const style = assembly('style');
+export const sub = assembly('sub');
+export const sup = assembly('sup');
+export const table = assembly('table');
+export const tbody = assembly('tbody');
+export const td = assembly('td');
+export const textarea = assembly('textarea');
+export const tfoot = assembly('tfoot');
+export const th = assembly('th');
+export const thead = assembly('thead');
+export const title = assembly('title');
+export const tr = assembly('tr');
+export const ul = assembly('ul');
+export const v = assembly('var');
+export const video = assembly('video');
 
-export var comment = (message) => {
+export const comment = (message) => {
     return ['comment', message];
+};
+export const text = (message) => {
+    return ['text', message];
+};
+export const loop = () => {
+    // Takes an object, array or JSON and loops though,
+    // Return the elements/ nodes to make up a list.
+    // you can nest loops.
 };
